1b619a8b236abb703425847ab492633b33b12125
u <marounhleyhel2@gmail.com>
2023-11-02 13:44:51 +0100

    ok

---
+++ app/pages/2_Contents.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import streamlit as st
+
+
+sns.set()
+
+st.set_page_config(
+    page_title="Exploration, data visualisation and data pre-processing",
+    page_icon="🌐")
+st.write(
+    """Introduction\n
+Technical Context	3\n
+Utilising Datasets	3\n
+Data Cleaning with Python	3\n
+Data Visualization Tools	4\n
+Project Objectives	4\n
+Technical Challenges	4\n
+Technical Opportunities	4\n
+Economic Context	5\n
+Economic Implications	5\n
+Scientific Context	6\n
+Climate Science and Data Analysis	6\n
+Understanding Climate Change	6\n
+Scientific Relevance	6\n
+Data-Driven Insights	6\n
+Contribution to Scientific Knowledge	6\n
+Our Roles	7\n
+Objectives	7\n
+Expertise Level of Group Members	7\n
+Understanding and Manipulation of Data	8\n
+Description of the Dataset(s) Used and Their Availability	8\n
+Ownership of Data	8\n
+Volume of the Dataset and Its Relevance to the Objectives	8\n
+Pre-Processing and Feature Engineering	9\n
+Data Cleaning and Processing Steps	9\n
+Data Cleaning	9\n
+Data Integration	10\n
+Feature Engineering	10\n
+Data Transformation	10\n
+Consideration of dimension reduction techniques	11\n
+Visualisations and Statistics	11\n
+Identification of relationships between variables	11\n
+Description of data distribution, outliers, etc.	12\n
+Data Distribution for Temperature Anomalies:	12\n
+Outliers	12\n
+Statistical Analyses Supporting Visual Findings	13\n
+"""
+)
