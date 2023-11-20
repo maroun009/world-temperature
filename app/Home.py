152464f40a6a4e1abd18c19b270950975834e13d
ChrisKoski94 <149513058+ChrisKoski94@users.noreply.github.com>
2023-11-20 19:09:03 +0100

    Update Home.py

--- app/Home.py
+++ app/Home.py
@@ -5,10 +5,14 @@ st.set_page_config(
     page_title="Climate Insights: A Comprehensive Analysis of Global Temperature Trends and Future Projections",
     page_icon="🌐",
 )
+
 st.markdown("# Climate Insights: A Comprehensive Analysis of Global Temperature Trends and Future Projections")
 
-greenhouse_gas = Image.open("app/img/GreenHouseGasesPollution.jpg")
+# Specify the correct path to the image
+image_path = "app/img/GreenHouseGasesPollution.jpg"
+greenhouse_gas = Image.open(image_path)
 st.image(greenhouse_gas, use_column_width=True)
 # Additional content goes here
+
 
 
