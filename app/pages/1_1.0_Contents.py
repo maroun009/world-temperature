67efc975c453aa9c0672881fe215b4418103d7e3
ChrisKoski94 <149513058+ChrisKoski94@users.noreply.github.com>
2023-11-20 19:14:47 +0100

    Update 1_1.0_Contents.py

--- app/pages/1_1.0_Contents.py
+++ app/pages/1_1.0_Contents.py
@@ -1,52 +1,16 @@
-import seaborn as sns
 import streamlit as st
-from PIL import Image
 
-sns.set()
+def presentation_breakdown_page():
+    st.markdown("# Presentation Breakdown")
 
-st.set_page_config(
-    page_title="Exploration, data visualisation and data pre-processing",
-    page_icon="🌐")
+    st.markdown("## Chris Kowalski")
+    st.write("Report 1 - Exploration, data visualization, and data pre-processing report")
 
-image = Image.open("app/img/GlobalWarmingTree.jpeg")
-st.image(image)
+    st.markdown("## Betsi Flores")
+    st.write("Report 2 – Modeling Report")
 
-st.markdown("""
-### Introduction
-* #### Technical Context
-    * ###### Utilising Datasets
-    * ###### Data Cleaning with Python
-    * ###### Data Visualization Tools
-    * ###### Project Objectives
-    * ###### Technical Challenges
-    * ###### Technical Opportunities
-* #### Economic Context
-    * ###### Economic Implications
-* #### Scientific Context
-    * ###### Climate Science and Data Analysis
-    * ###### Understanding Climate Change
-    * ###### Scientific Relevance
-    * ###### Data-Driven Insights
-    * ###### Contribution to Scientific Knowledge
-* #### Our Roles
-    * ###### Objectives
-    * ###### Expertise Level of Group Members
-### Understanding and Manipulation of Data
-* #### Description of the Dataset(s) Used and Their Availability
-* #### Ownership of Data
-* #### Volume of the Dataset and Its Relevance to the Objectives
-### Pre-Processing and Feature Engineering
-* #### Data Cleaning and Processing Steps
-    * ###### Data Cleaning
-    * ###### Data Integration
-    * ###### Feature Engineering
-    * ###### Data Transformation
-    * ###### Consideration of dimension reduction techniques
-### Visualisations and Statistics
-* #### Identification of relationships between variables
-* #### Description of data distribution, outliers, etc.
-    * ###### Data Distribution for Temperature Anomalies
-    * ###### Outliers
-* #### Statistical Analyses Supporting Visual Findings
-"""
-)
+    st.markdown("## Maroun Hleihel")
+    st.write("Report 3 - Challenges and Insights in Climate Modeling")
+
+if __name__ == "__main__":
+    presentation_breakdown_page()
