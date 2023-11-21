8294409c008f253f2a9d19ee337c2fbc86c0218f
ChrisKoski94 <149513058+ChrisKoski94@users.noreply.github.com>
2023-11-21 09:26:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,21 @@
 # streamlit_app.py
 import streamlit as st
-from pages.home import home_page  # Importing the home page function
+from pages import project_breakdown,
 
+# Create a dictionary to map page names to their corresponding functions
+pages = {
+    
+    "Project Breakdown": project_breakdown.project_breakdown_page,
+}
+
+# Streamlit app
 def main():
-    st.set_page_config(
-        page_title="ClimateTemp Dashboard",
-        page_icon=":partly_sunny:",
-        layout="wide"
-    )
-
-    st.title("ClimateTemp Dashboard")
-
-    # Create a sidebar for navigation
     st.sidebar.title("Navigation")
-    pages = {
-        "Home": home_page,
-        # Add more pages here as your app grows
-    }
-    selected_page = st.sidebar.radio("Go to", list(pages.keys()))
+    page_selection = st.sidebar.radio("Go to", list(pages.keys()))
 
     # Display the selected page
-    pages[selected_page]()
+    pages[page_selection]()
 
 if __name__ == "__main__":
     main()
+
